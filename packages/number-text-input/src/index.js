216b57d398811a840532d4310e2c1aa700324335
abz53378 <a53378@yahoo.com.tw>
2017-12-27 10:06:21 +0800

    number text input

---
+++ packages/number-text-input/src/index.js
@@ -0,0 +1,24 @@
+// @flow
+
+import React, { Component } from "react";
+import { Input } from "antd";
+
+type Props = defaultProps & {
+  value: string
+};
+
+export default class NumberInput extends Component<Props> {
+  onChange = (e: any) => {
+    const value = parseInt(e.target.value) || 0;
+    this.props.onChange(this.props.id, "update", value);
+  };
+
+  render() {
+    const { value } = this.props;
+    return (
+      <div id="input">
+        <Input type="text" value={value} onChange={this.onChange} />
+      </div>
+    );
+  }
+}
