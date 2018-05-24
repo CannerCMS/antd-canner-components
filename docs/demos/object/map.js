4cc1c5ce533ab70641ea01297b8cf5d7983f0c31
Siou <abz53378@gmail.com>
2018-05-24 12:15:46 +0800

    docs for map

--- docs/demos/object/map.js
+++ docs/demos/object/map.js
@@ -8,8 +8,8 @@ import type {PrimitiveTypes} from '../types';
 import RefId from 'canner-ref-id';
 
 @ExamplePrimitiveValueWrapper(fromJS({
-  key: "Eiflj7DngaPmlrDljJfluILmlrDojorljYDmgJ3mupDot68yMzDlt7c",
-  label: "台灣新北市新莊區思源路230巷",
+  placeId: "Eiflj7DngaPmlrDljJfluILmlrDojorljYDmgJ3mupDot68yMzDlt7c",
+  address: "台灣新北市新莊區思源路230巷",
   lat: 25.0489013,
   lng: 121.46126290000007
 }))
