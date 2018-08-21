8598d67ddc082a405784b310e12c239723b5b135
chilijung <chilijung@gmail.com>
2018-08-21 15:17:21 +0800

    fix editor in ssr

--- packages/antd-object-editor/src/index.js
+++ packages/antd-object-editor/src/index.js
@@ -1,58 +1,19 @@
 // @flow
-
 import React, { PureComponent } from "react";
-import ReactQuill from 'react-quill';
 import NoSSR from 'react-no-ssr';
+import Editor from './editor';
 
 // type
 import type {ObjectDefaultProps} from 'types/ObjectDefaultProps';
 
 type Props = ObjectDefaultProps
 
-type State = {
-  value: string
-}
 
-const modules = {
-  toolbar: [
-    [{ 'header': [1, 2, false] }],
-    ['bold', 'italic', 'underline','strike', 'blockquote'],
-    [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-    ['link', 'image'],
-    ['clean']
-  ],
-}
-
-const formats = [
-  'header',
-  'bold', 'italic', 'underline', 'strike', 'blockquote',
-  'list', 'bullet', 'indent',
-  'link', 'image'
-]
-
-export default class Editor extends PureComponent<Props, State> {
-  htmlKey: string;
-
-  constructor(props: Props) {
-    super(props);
-    this.htmlKey = 'html';
-  }
-
-  handleChange = (value: string) => {
-    const {refId, onChange} = this.props;
-
-    onChange(refId, 'update', {[this.htmlKey]: value});
-  }
-
+export default class EditorContainer extends PureComponent<Props, {}> {
   render() {
-    const {value} = this.props;
     return (
       <NoSSR>
-        <ReactQuill
-          modules={modules}
-          formats={formats}
-          defaultValue={value && value[this.htmlKey]}
-          onChange={this.handleChange} />
+        <Editor {...this.props}/>
       </NoSSR>
     )
   }
