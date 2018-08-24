0a3f14173610a2784e1add7f38537996b9529eba
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-08-24 13:11:24 +0800

    Add antd-indicator-list

---
+++ packages/antd-indicator-list/src/index.js
@@ -0,0 +1,43 @@
+// @flow
+import * as React from 'react';
+import { List } from 'antd';
+
+// type
+import type {IndicatorDefaultProps} from 'types/IndicatorDefaultProps';
+
+type Props = IndicatorDefaultProps & {
+  uiParams: {
+    avatar: (value: any) => React.Node,
+    title: (value: any) => React.Node,
+    description: (value:any) => React.Node,
+    content: (value: any) => React.Node
+  }
+};
+
+const getRandomKey = () => (Math.random().toString(36).substr(2, 10));
+
+const IndicatorList = ({ value, uiParams: {
+  avatar,
+  title,
+  description,
+  content
+} }: Props) => {
+  return (
+    <List
+      dataSource={value}
+      renderItem={item => (
+        <List.Item key={item.id || getRandomKey()}>
+          <List.Item.Meta
+            avatar={avatar(item)}
+            title={title(item)}
+            description={description(item)}
+          />
+          { content(item) }
+        </List.Item>
+      )}
+    >
+  </List>
+  );
+}
+
+export default IndicatorList;
