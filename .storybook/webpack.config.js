3da0e7005d30756c93931675f36637305ccb08a1
Siou <abz53378@gmail.com>
2018-10-11 11:03:36 +0800

    add less and less-loader in webpack.config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -37,6 +37,23 @@ module.exports = (storybookBaseConfig, configType) => {
           loader: "css-loader"
         }
       ]
+    },
+    {
+      test: /\.less$/,
+      use: [
+        {
+          loader: "style-loader"
+        },
+        {
+          loader: "css-loader"
+        },
+        {
+          loader: "less-loader",
+          options: {
+            javascriptEnabled: true
+          }
+        }
+      ]
     }
   ];
 
