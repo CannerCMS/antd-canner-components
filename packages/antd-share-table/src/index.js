5dcab03ca8d1e9a45db40824d90217f8456bcdfb
chilijung <chilijung@gmail.com>
2019-03-08 10:28:48 +0800

    update share table to hooks

--- packages/antd-share-table/src/index.js
+++ packages/antd-share-table/src/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Table, Button, Icon, Popconfirm } from "antd";
 import {get} from 'lodash';
 import { FormattedMessage } from "react-intl";
@@ -43,175 +43,160 @@ type Props = ArrayDefaultProps<FieldItem> & {
   reset: Function
 };
 
-type State = {
-  showAddModal: boolean
-}
+const TableArrayPlugin = (props: Props) => {
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
 
-export default @injectIntl class TableArrayPlugin extends Component<Props, State> {
-  static defaultProps = {
-    value: [],
-    showPagination: true
+  const onSelectChange = (selectedRowKeys) => {
+    setSelectedRowKeys(selectedRowKeys);
+  }
+
+  const {
+    refId,
+    uiParams,
+    onChange,
+    items,
+    deploy,
+    intl,
+    reset,
+    toolbar,
+    goTo,
+    rootValue,
+    request,
+    keyName,
+    disabled = {},
+    value= []
+  } = props;
+
+  const addText = (
+    <FormattedMessage
+      id="array.table.addText"
+      defaultMessage={defaultMessage.en["array.table.addText"]}
+    />
+  );
+  let {
+    columns = [],
+    bordered,
+    components,
+    footer,
+    showHeader = true,
+    size = 'default',
+    createButtonPosition = 'right',
+  } = uiParams;
+  let disableCreate = false;
+  let disableUpdate = false;
+  let disableDelete = false;
+  if (typeof disabled === 'boolean') {
+    if (disabled) {
+      disableCreate = true;
+      disableUpdate = true;
+      disableDelete = true;
+    }
+  } else if (typeof disabled === 'object') {
+    disableCreate = disabled.create;
+    disableUpdate = disabled.update;
+    disableDelete = disabled.delete;
+  }
+  const newColumns = columns.map(column => {
+    return {...column, title: getIntlMessage(intl, column.title)};
+  });
+  const newColumnsRender = renderValue(newColumns, items.items, {
+    refId,
+    onChange,
+    uiParams,
+    goTo,
+    reset,
+    deploy
+  });
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
   };
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedRowKeys: []
-    }
-  }
+  const selectedValue = value.filter(item => selectedRowKeys.indexOf(item.id) > -1);
 
-  onSelectChange = (selectedRowKeys) => {
-    this.setState({selectedRowKeys});
-  }
-
-  render() {
-    const {
-      refId,
-      uiParams,
-      onChange,
-      items,
-      deploy,
-      intl,
-      reset,
-      toolbar,
-      goTo,
-      rootValue,
-      request,
-      keyName,
-      disabled = {},
-      value
-    } = this.props;
-    const {
-      selectedRowKeys
-    } = this.state;
-
-    const addText = (
-      <FormattedMessage
-        id="array.table.addText"
-        defaultMessage={defaultMessage.en["array.table.addText"]}
-      />
-    );
-    let {
-      columns = [],
-      bordered,
-      components,
-      footer,
-      showHeader = true,
-      size = 'default',
-      createButtonPosition = 'right',
-    } = uiParams;
-    let disableCreate = false;
-    let disableUpdate = false;
-    let disableDelete = false;
-    if (typeof disabled === 'boolean') {
-      if (disabled) {
-        disableCreate = true;
-        disableUpdate = true;
-        disableDelete = true;
-      }
-    } else if (typeof disabled === 'object') {
-      disableCreate = disabled.create;
-      disableUpdate = disabled.update;
-      disableDelete = disabled.delete;
-    }
-    const newColumns = columns.map(column => {
-      return {...column, title: getIntlMessage(intl, column.title)};
-    });
-    const newColumnsRender = renderValue(newColumns, items.items, {
-      refId,
-      onChange,
-      uiParams,
-      goTo,
-      reset,
-      deploy
-    });
-    const rowSelection = {
-      selectedRowKeys,
-      onChange: this.onSelectChange,
-    };
-    const selectedValue = value.filter(item => selectedRowKeys.indexOf(item.id) > -1);
-
-    const recordValue = get(rootValue, refId.remove().getPathArr());
-    if (!disableUpdate || !disableDelete) {
-      newColumnsRender.push({
-        title: intl.formatMessage({ id: "array.table.actions" }),
-        dataIndex: "__settings",
-        key: "__settings",
-        render: (text, record) => {
-          return (
-            <ButtonGroup>
-              {!disableUpdate && (
-                <Button
-                  icon="edit"
-                  data-testid="edit-button"
-                  onClick={() => this.props.update(text, record)}
-                />
-              )}
-              {!disableDelete && (
-                <Popconfirm
-                  title={intl.formatMessage({ id: "array.table.delete.confirm" })}
-                  onConfirm={() => this.props.delete(text, record)}
-                  okType="danger"
-                  testid="delete-popconfirm"
-                >
-                  <Button
-                    icon="delete"
-                    data-testid="delete-button"
-                  />
-                </Popconfirm>
-              )}
-            </ButtonGroup>
-          );
-        }
-      });
-    }
-    return (
-      <Toolbar
-        toolbar={toolbar}
-        dataSource={value}
-        recordValue={recordValue}
-        selectedValue={selectedValue}
-        items={items}
-        keyName={keyName}
-        request={request}
-        deploy={deploy}
-      >
-      {
-        ({value, showPagination}) => {
-          return (
-            <React.Fragment>
-              {!disableCreate && (
-                <Button
-                  type="primary"
-                  style={{
-                    marginBottom: '10px',
-                    marginLeft: createButtonPosition === 'right' ? 'auto' : 0,
-                    display: 'block'
-                  }}
-                  data-testid="add-button"
-                  onClick={this.props.create}
-                >
-                  <Icon type="plus" />{addText}
-                </Button>
-              )}
-              <Table
-                rowSelection={get(toolbar, 'actions.export') ? rowSelection : undefined}
-                pagination={showPagination}
-                dataSource={value}
-                columns={newColumnsRender}
-                scroll={{ x: true }}
-                rowKey="id"
-                bordered={bordered}
-                components={components}
-                footer={footer}
-                showHeader={showHeader}
-                size={size}
+  const recordValue = get(rootValue, refId.remove().getPathArr());
+  if (!disableUpdate || !disableDelete) {
+    newColumnsRender.push({
+      title: intl.formatMessage({ id: "array.table.actions" }),
+      dataIndex: "__settings",
+      key: "__settings",
+      render: (text, record) => {
+        return (
+          <ButtonGroup>
+            {!disableUpdate && (
+              <Button
+                icon="edit"
+                data-testid="edit-button"
+                onClick={() => this.props.update(text, record)}
               />
-            </React.Fragment>
-          )
-        }
+            )}
+            {!disableDelete && (
+              <Popconfirm
+                title={intl.formatMessage({ id: "array.table.delete.confirm" })}
+                onConfirm={() => this.props.delete(text, record)}
+                okType="danger"
+                testid="delete-popconfirm"
+              >
+                <Button
+                  icon="delete"
+                  data-testid="delete-button"
+                />
+              </Popconfirm>
+            )}
+          </ButtonGroup>
+        );
       }
-      </Toolbar>
-    );
+    });
   }
+  return (
+    <Toolbar
+      toolbar={toolbar}
+      dataSource={value}
+      recordValue={recordValue}
+      selectedValue={selectedValue}
+      items={items}
+      keyName={keyName}
+      request={request}
+      deploy={deploy}
+    >
+    {
+      ({value, showPagination}) => {
+        return (
+          <React.Fragment>
+            {!disableCreate && (
+              <Button
+                type="primary"
+                style={{
+                  marginBottom: '10px',
+                  marginLeft: createButtonPosition === 'right' ? 'auto' : 0,
+                  display: 'block'
+                }}
+                data-testid="add-button"
+                onClick={this.props.create}
+              >
+                <Icon type="plus" />{addText}
+              </Button>
+            )}
+            <Table
+              rowSelection={get(toolbar, 'actions.export') ? rowSelection : undefined}
+              pagination={showPagination}
+              dataSource={value}
+              columns={newColumnsRender}
+              scroll={{ x: true }}
+              rowKey="id"
+              bordered={bordered}
+              components={components}
+              footer={footer}
+              showHeader={showHeader}
+              size={size}
+            />
+          </React.Fragment>
+        )
+      }
+    }
+    </Toolbar>
+  );
+
 }
+
+
+export default injectIntl(TableArrayPlugin)
